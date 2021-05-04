67aefddfc76ddd2b2d5c0c4973ca1f61a7d36494
Krishna <inbox.krishna95@gmail.com>
2021-05-04 11:10:52 +0530

    add cache hit/miss functionality

--- src/api/v1/controllers/cacheController.js
+++ src/api/v1/controllers/cacheController.js
@@ -1,23 +1,54 @@
 import Cache from "../models/cache.js"
+import { uuid } from "uuidv4"
 
 const cacheController = {}
 
+const createDummyCache = async (key) => {
+  try {
+    // check for overflow
+    
+    const cacheItem = new Cache({
+      key,
+      value: uuid(), // uuid to generate unique random values
+      lastAccess: Date.now()
+    })
+
+    return await cacheItem.save()
+  } catch (err) {
+    console.log(err)
+  }
+}
+
 cacheController.getAllCacheItems = async (req, res) => {
-  const cacheItems = await Cache.find()
-  res.status(200).json(cacheItems)
+  try {
+    const cacheItems = await Cache.find()
+    res.status(200).json(cacheItems)
+  } catch(err) {
+    res.status(500).json({ message: err.message})
+  }
+  
 }
 
 cacheController.getCacheItemById = async(req, res) => {
   const id = req.params.id;
-  let cacheItem = Cache.findOne({ key: id})
+  let cacheItem = await Cache.findOne({ key: id})
 
+  // if no cacheItem create a new random dummy
   if (!cacheItem) {
     console.log("Cache Miss")
+    // create new random string for the key
+    const newCache = await createDummyCache(id)
+    // update cache & return
+    res.status(201).send(newCache.value)
     return;
   }
 
+  // check for expired Items
+
   console.log("Cache Hit")
   res.status(200).send(cacheItem.value)
+
+  // reset ttl on every read/cache hit
 }
 
 export default cacheController
